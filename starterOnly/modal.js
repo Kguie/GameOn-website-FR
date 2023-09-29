5978a8a5bc5590b32f3ac7700452d8b8e4bcdaa9
Kevin Guieba <kevin@sofy.fr>
2023-09-28 23:50:37 -0400

    Mise en place des fichiers, correction des erreurs pour l'affichage du titre et du texte dans la hero-section sur petit écran, correction d'une erreur de scroll lors de l'ouverture de la modale aussi sur petit écran, ajout de la fonction permettant de fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn= document.querySelector(".close");
+const heroSection= document.querySelector(".hero-section");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +20,19 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  // Fix scrolling error for mobile size screens
+  heroSection.style.overflow="hidden";
+  heroSection.style.position="fixed";
 }
 
+// close modal event
+closeModalBtn.addEventListener("click", closeModal);
+
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+  // Fix scrolling error for mobile size screens
+  heroSection.style.overflow="auto";
+  heroSection.style.position="static";
+}
 
